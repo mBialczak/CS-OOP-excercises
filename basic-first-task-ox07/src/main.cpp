dd0707e7fb3de135719c845d418cf6c90fd31811
Maciej Białczak <m.bialczak@protonmail.com>
2022-05-03 18:07:43 +0200

    Added printout for empty ship values

--- basic-first-task-ox07/src/main.cpp
+++ basic-first-task-ox07/src/main.cpp
@@ -1,6 +1,15 @@
+#include "Ship.hpp"
 
+#include <iostream>
 
 int main([[maybe_unused]] int argc, [[maybe_unused]] const char* argv[])
 {
+    Ship empty_ship;
+    std::cout << "Empty ship ID:\t" << empty_ship.id() << "\n"
+              << "Empty ship name:\t" << empty_ship.name() << "\n"
+              << "Empty ship speed:\t" << empty_ship.speed() << "\n"
+              << "Empty ship maxCrew:\t" << empty_ship.maxCrew() << "\n"
+              << "Empty ship capacity:\t" << empty_ship.capacity() << std::endl;
+
     return 0;
 }
