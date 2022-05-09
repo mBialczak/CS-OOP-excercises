536e20cb6ed47898fa70a77cb9ac6bab6559a44a
Maciej Białczak <m.bialczak@protonmail.com>
2022-05-09 20:57:46 +0200

    Added amount and getter to Cargo class

--- basic-tasks/src/Cargo.cpp
+++ basic-tasks/src/Cargo.cpp
@@ -1,10 +1,16 @@
 #include "Cargo.hpp"
 
-Cargo::Cargo(std::string name)
+Cargo::Cargo(std::string name, int amount_kg)
     : name_(name)
+    , amount_(amount_kg)
 { }
 
 std::string Cargo::name() const
 {
     return name_;
+}
+
+int Cargo::amount() const
+{
+    return amount_;
 }
--- basic-tasks/src/Cargo.hpp
+++ basic-tasks/src/Cargo.hpp
@@ -5,9 +5,12 @@
 class Cargo
 {
   public:
-    Cargo(std::string name);
+    Cargo(std::string name, int amount_kg);
+    // getters
     std::string name() const;
+    int amount() const;
 
   private:
-    std::string name_;
+    std::string name_;   // name of the cargo
+    int amount_;   // amount in kg
 };
