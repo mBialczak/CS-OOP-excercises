7a5fab8f21e73f1e36fb70ea391a77386f8e8be0
Maciej Białczak <m.bialczak@protonmail.com>
2022-07-21 21:51:14 +0200

    implementation done

--- Coordinates/Coordinates.cpp
+++ Coordinates/Coordinates.cpp
@@ -1,5 +1,21 @@
 #include "Coordinates.hpp"
 
+#include <cmath>
+
 namespace coords {
+
+Coordinates::Coordinates(double positionX, double positionY)
+    : x_(positionX)
+    , y_(positionY)
+{
+}
+
+double Coordinates::distance(const Coordinates& first, const Coordinates& second)
+{
+    double xDifference = second.x_ - first.x_;
+    double yDifference = second.y_ - first.y_;
+
+    return sqrt(pow(xDifference, 2) + pow(yDifference, 2));
+}
 
 }   // namespace coords
--- Coordinates/Coordinates.hpp
+++ Coordinates/Coordinates.hpp
@@ -2,4 +2,14 @@
 
 namespace coords {
 
+struct Coordinates
+{
+    Coordinates(double positionX, double positionY);
+    static double distance(const Coordinates& first, const Coordinates& second);
+    bool operator==(const Coordinates& otherPosition) const = default;
+
+    double x_;
+    double y_;
+};
+
 }   // namespace coords
